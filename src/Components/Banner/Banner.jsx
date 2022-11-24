d95aee39de46c90114162beb9278bec4ccac0a8a
Ashikur_Rahman <ashikur35-540@diu.edu.bd>
2022-11-24 18:15:27 +0600

    hero added

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,40 +1,43 @@
-import React from 'react'
+import heroImg from "../../Assets/Carsousel/c1.jpg";
 
-const Banner = () => {
+export const Banner = () => {
     return (
-        <>
-            <div className="carousel w-[90%] min-h-screen mx-auto mt-10 rounded-md">
-                <div id="slide1" className="carousel-item relative w-full">
-                    <img src="https://placeimg.com/800/200/arch" className="w-full object-cover" />
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide4" className="btn btn-circle">❮</a>
-                        <a href="#slide2" className="btn btn-circle">❯</a>
+        <div className="relative flex flex-col py-16 lg:pt-0 lg:flex-col lg:pb-0">
+            <div className="flex flex-col items-start w-full max-w-xl px-4 mx-auto lg:px-8 lg:max-w-screen-xl">
+                <div className="mb-16 lg:my-40 lg:max-w-lg lg:pr-5">
+                    <div className="max-w-xl mb-6">
+                        <div>
+                            <p className="inline-block  py-px mb-4 text-xs font-semibold tracking-wider text-secondary uppercase rounded-full bg-teal-accent-400">
+                                New Collection arrives
+                            </p>
+                        </div>
+                        <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-primary sm:text-4xl sm:leading-none md:leading-normal">
+                            Fullfill
+                            <br className="hidden md:block" />
+                            Your dream{' '}
+                            <span className="inline-block text-deep-purple-accent-400">
+                                with low cost
+                            </span>
+                        </h2>
+                        <p className="text-base font-light text-slate-200 md:text-lg">
+                            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+                            quae. explicabo.
+                        </p>
                     </div>
-                </div>
-                <div id="slide2" className="carousel-item relative w-full">
-                    <img src="https://placeimg.com/800/200/arch" className="w-full" />
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide1" className="btn btn-circle">❮</a>
-                        <a href="#slide3" className="btn btn-circle">❯</a>
-                    </div>
-                </div>
-                <div id="slide3" className="carousel-item relative w-full">
-                    <img src="https://placeimg.com/800/200/arch" className="w-full" />
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide2" className="btn btn-circle">❮</a>
-                        <a href="#slide4" className="btn btn-circle">❯</a>
-                    </div>
-                </div>
-                <div id="slide4" className="carousel-item relative w-full">
-                    <img src="https://placeimg.com/800/200/arch" className="w-full" />
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide3" className="btn btn-circle">❮</a>
-                        <a href="#slide1" className="btn btn-circle">❯</a>
+                    <div className="flex flex-col items-center md:flex-row">
+                        <button className="btn glass">Explore</button>
+
                     </div>
                 </div>
             </div>
-        </>
-    )
-}
-
-export default Banner
+            <div className="inset-y-0 right-0 w-full max-w-xl px-4 mx-auto lg:pl-8 lg:pr-0 lg:mb-0 lg:mx-0 lg:w-1/2 lg:max-w-full lg:absolute xl:px-0">
+                <img
+                    className="object-cover w-full h-56 rounded shadow-lg lg:rounded-none lg:shadow-none sm:h-96 lg:h-full"
+                    src={heroImg}
+                    alt=""
+                />
+            </div>
+        </div>
+    );
+};
